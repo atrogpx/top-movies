d1f1553a4467dc2d60a2cef3cdc66772efbfac4f
Atro Zare'i <103967835+atrogpx@users.noreply.github.com>
2023-10-26 19:54:06 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "lxml")
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in soup.find_all(name="h3", class_="title")]
+movies = movie_titles[::-1]
+
+with open("movies.txt", "w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
